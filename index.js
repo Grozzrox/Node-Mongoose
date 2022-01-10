11a3394977f24d3cfc0299c3b24ea8d83bcaacf4
Andrew <grozzy09@gmail.com>
2022-01-09 21:57:04 -0500

    Pole Green Park

--- index.js
+++ index.js
@@ -17,7 +17,29 @@ connect.then(() => {
         description: 'test'
     });
 
+    const oldCampsite = new Campsite({
+        name: 'Pole Green Park',
+        description: 'The coolest park this side of the Cold Harbor River!'
+    });
+
     newCampsite.save()
+    .then(campsite => {
+        console.log(campsite);
+        return Campsite.find();
+    })
+    .then(campsites => {
+        console.log(campsites);
+        return Campsite.deleteMany();
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch(err => {
+        console.log(err);
+        mongoose.connection.close();
+    });
+
+    oldCampsite.save()
     .then(campsite => {
         console.log(campsite);
         return Campsite.find();
